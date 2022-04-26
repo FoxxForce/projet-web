43b5966b475e11cb8e604f660b3df75558150308
SOUMARE cheikou <cheikou.s99@gmail.com>
2022-04-26 16:24:34 +0200

    modification popup

--- public/script.js
+++ public/script.js
@@ -47,45 +47,65 @@ $(document).ready(function() {
       console.log(data);
       for(let i=0; i<data.length; i++){
         elphoto = '<button id="' + data[i]['nom'] +'" type="button">' +data[i]['nom']+ 'menu</button>';
-        elphoto += '<ul id="liste'+ data[i]['nom'] +'">'
-        elphoto += '<li class="popupButton ajoutPizza">Ajout pizza</li>'
-        elphoto += '<li class="popupButton ajoutEntree">Ajout  entrée</li>'
-        elphoto += '<li class="popupButton ajoutBoisson">Ajout boisson</li></ul>'
         console.log(elphoto);
         $('body').append(elphoto);
         $('#liste'+data[i]['nom']).hide();
         $('#'+data[i]['nom']).click(function() {
-          $('#liste'+data[i]['nom']).slideToggle();
-      });
-        $('#liste'+data[i]['nom']+' .ajoutPizza').click(function(){
           $('#popup').show();
           $('.pizza').show();
+          $('.ajoutPizza').hide();
+          $('.ajoutBoisson').hide();
+          $('.ajoutEntree').show();
+          $('.confirmer').hide();
           $('.pizza .choix').off().on('change', function() {
-            console.log("okok" +data[i]['nb_pizza']);
             if($('.pizza input[type=checkbox]:checked').length > data[i]['nb_pizza']) {
                 this.checked = false;
             }
           });
-        });
-        $('#liste'+data[i]['nom']+' .ajoutEntree').click(function(){
-          $('#popup').show();
-          $('.entree').show();
-          $('.entree .choix').off().on('change', function() {
-            console.log("okok" +data[i]['entree']);
-            if($('.entree input[type=checkbox]:checked').length > data[i]['nb_entree']) {
-                this.checked = false;
-            }
+          $('.ajoutPizza').click(function(){
+            $('#popup .container-fluid').hide();
+            $('.ajoutPizza').hide();
+            $('.ajoutBoisson').hide();
+            $('.ajoutEntree').show();
+            $('#popup').show();
+            $('.pizza').show();
+            $('.confirmer').hide();
+            $('.pizza .choix').off().on('change', function() {
+              if($('.pizza input[type=checkbox]:checked').length > data[i]['nb_pizza']) {
+                  this.checked = false;
+              }
+            });
           });
-        });
-        $('#liste'+data[i]['nom']+' .ajoutBoisson').click(function(){
-          $('#popup').show();
-          $('.boisson').show();
-          $('.boisson .choix').off().on('change', function() {
-            if($('.boisson input[type=checkbox]:checked').length > data[i]['nb_boisson']) {
-                this.checked = false;
-            }
+          $('.ajoutEntree').click(function(){
+            $('#popup .container-fluid').hide();
+            $('.ajoutPizza').show();
+            $('.confirmer').hide();
+            $('.ajoutBoisson').show();
+            $('.ajoutEntree').hide();
+            $('#popup').show();
+            $('.entree').show();
+            $('.entree .choix').off().on('change', function() {
+              if($('.entree input[type=checkbox]:checked').length > data[i]['nb_entree']) {
+                  this.checked = false;
+              }
+            });
           });
-        });
+          $('.ajoutBoisson').click(function(){
+            $('.confirmer').show();
+            $('#popup .container-fluid').hide();
+            $('.ajoutPizza').hide();
+            $('.ajoutBoisson').hide();
+            $('.ajoutEntree').show();
+            $('#popup').show();
+            $('.boisson').show();
+            $('.boisson .choix').off().on('change', function() {
+              if($('.boisson input[type=checkbox]:checked').length > data[i]['nb_boisson']) {
+                  this.checked = false;
+              }
+            });
+          });
+      });
+        
       }
 
 
